a075a901b57124b9ca1c0cc1b63b45316722cfc4
prateek-sz <prateek.rathore@synerzip.com>
2021-06-04 15:34:19 +0530

    link issue fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/Link';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>Top Trending Things</title>
         <meta name="description" content="Latest top trending things" />
-        <link rel="icon" href="/favicon.ico" />
+        <Link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main
