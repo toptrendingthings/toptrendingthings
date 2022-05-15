c2852695702cd5ba31faec46be3ea3f998038d43
prateek-sz <prateek.rathore@synerzip.com>
2022-05-16 00:29:55 +0530

    Update node version

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
         loader: 'akamai',
         path: '',
     },
+    future: {
+        webpack5: true,
+    },
+    webpack: function (config, options) {
+        config.experiments = {};
+        return config;
+    },
 }
